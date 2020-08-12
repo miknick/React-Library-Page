8fc845d56dfe3e57ee8ce3968a83274effab1613
Yunus Akten <yunusakten2@gmail.com>
2020-08-12 16:59:11 +0300

    Updates

---
+++ precache-manifest.be8b5a9923e135eac33dfe7ada6cfe64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e18c6c0785186310b8b212dead4499",
+    "url": "/React-Library-Page/index.html"
+  },
+  {
+    "revision": "35475075e85150808277",
+    "url": "/React-Library-Page/static/css/main.4e2c189d.chunk.css"
+  },
+  {
+    "revision": "2c6d7f94149a4a5cd6cc",
+    "url": "/React-Library-Page/static/js/2.bd27e164.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Library-Page/static/js/2.bd27e164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35475075e85150808277",
+    "url": "/React-Library-Page/static/js/main.42e35d18.chunk.js"
+  },
+  {
+    "revision": "a9911bf95949858200e9",
+    "url": "/React-Library-Page/static/js/runtime-main.0bd939e2.js"
+  },
+  {
+    "revision": "89616ab6209aac44d2897b102c6f9e0d",
+    "url": "/React-Library-Page/static/media/background.89616ab6.jpg"
+  }
+]);
